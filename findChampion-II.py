767bf0bd1d3c00b53242ca82339f69ee032c29ba
Sharad <sharad011989@gmail.com>
2024-11-27 09:24:05 +0530

    find chanpion II

---
+++ findChampion-II.py
@@ -0,0 +1,15 @@
+#2924. Find Champion II
+
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        incoming = [0] * n
+        for src, dst in edges:
+            incoming[dst] += 1
+
+        champions = []
+        for i, incoming_cnt in enumerate(incoming):
+            if not incoming_cnt:
+                champions.append(i)
+        if len(champions) > 1:
+            return -1
+        return champions[0]
